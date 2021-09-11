2d2ad6c16d6ee8b4689367104e82655373449f41
Kasun <Kasunrandima245@gmail.com>
2021-09-11 16:34:20 +0530

    change mail

--- application/views/addname.php
+++ application/views/addname.php
@@ -42,7 +42,7 @@
       </div>
 
       <div class="form-group">
-        <button type="submit" class="btn btn-success">Add Name & emaileeeeeeeee22222222222222222222222222222222222222222222222</button>
+        <button type="submit" class="btn btn-success">Add Name & emaile333333333333333333333333333333</button>
       </div>
     </form>
   </div>
